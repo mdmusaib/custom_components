16e2eea4b5362c04ad2c988bd09f72f94775ec69
mdmusaib <musaibkm@gmail.com>
2019-10-10 14:08:03 +0530

    Updates

---
+++ precache-manifest.52fb052e633fa5d2a4883632ba2fb559.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "566338bccbbc056369bf585cb442ae5e",
+    "url": "/custom_components/index.html"
+  },
+  {
+    "revision": "b681bf925f844fcefa4f",
+    "url": "/custom_components/static/css/2.612dcc5b.chunk.css"
+  },
+  {
+    "revision": "b192958aa1e828e01d3b",
+    "url": "/custom_components/static/css/main.f1c41460.chunk.css"
+  },
+  {
+    "revision": "b681bf925f844fcefa4f",
+    "url": "/custom_components/static/js/2.90d52458.chunk.js"
+  },
+  {
+    "revision": "b192958aa1e828e01d3b",
+    "url": "/custom_components/static/js/main.855a751b.chunk.js"
+  },
+  {
+    "revision": "0aff6038237c2e3036e5",
+    "url": "/custom_components/static/js/runtime-main.f0798a20.js"
+  }
+]);

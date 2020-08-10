7f84c4cc277dff96fd79738e666c37b6cd1ad1e6
Mohammed Musaib <mohammed.musaib@outlook.com>
2020-08-10 21:46:11 +0530

    Updates

---
+++ precache-manifest.7b35ef5fe0db12a88c6edcbe0bbf1b1b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "789530e2fc4c761569c9efcee84b1aa0",
+    "url": "/react-components/index.html"
+  },
+  {
+    "revision": "af748334bfdd3892d3c7",
+    "url": "/react-components/static/css/2.a2e1afe8.chunk.css"
+  },
+  {
+    "revision": "dbf82bd8dc45ddb4a34e",
+    "url": "/react-components/static/css/main.41b7f8f1.chunk.css"
+  },
+  {
+    "revision": "af748334bfdd3892d3c7",
+    "url": "/react-components/static/js/2.058ae983.chunk.js"
+  },
+  {
+    "revision": "dbf82bd8dc45ddb4a34e",
+    "url": "/react-components/static/js/main.f95596e1.chunk.js"
+  },
+  {
+    "revision": "8136ea51e215d7b36c46",
+    "url": "/react-components/static/js/runtime-main.27c2075f.js"
+  },
+  {
+    "revision": "71e4862eef32c43eca3f345266d3b189",
+    "url": "/react-components/static/media/dxicons.71e4862e.woff2"
+  },
+  {
+    "revision": "dffa1b6f8175e126e4fd38e5a9091fca",
+    "url": "/react-components/static/media/dxicons.dffa1b6f.woff"
+  },
+  {
+    "revision": "e7b052cb56375958973bf0743cb5d36e",
+    "url": "/react-components/static/media/dxicons.e7b052cb.ttf"
+  },
+  {
+    "revision": "6a6b95a38be24bc7f0568161d0a37a36",
+    "url": "/react-components/static/media/preloader.6a6b95a3.gif"
+  }
+]);

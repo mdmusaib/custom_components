a247a8c349599a86bed24524ac7eb8cc38e49118
Mohammed Musaib <mohammed.musaib@outlook.com>
2020-08-10 21:51:56 +0530

    changesdone

--- src/components/gpGridLayout.js
+++ src/components/gpGridLayout.js
@@ -1,131 +1,220 @@
-import React from 'react';
-import '../gridLayout.css'
-import img_avatar from  '../img_avatar.jpg';
+import React from "react";
+import "../gridLayout.css";
+import img_avatar from "../img_avatar.jpg";
 
+const GpGridLayout = (props) => {
+  const handleClick = (value) => {
+    props.onClick(value);
+  };
 
-const GpGridLayout=(props)=>{
-  
-  const handleClick=(value)=>{
-      
-  props.onClick(value);
-  }
+  return (
+    <React.Fragment>
+      <div className="wrapper">
+        <div
+          class="card"
+          onClick={() => {
+            handleClick("Designer");
+          }}
+          title="Designer"
+        >
+          <img class="card-img-top" src={img_avatar} alt="Avatar" />
+          <div class="card-body p-0 m-0">
+            <p class="text-center">
+              <span>
+                <b>TEST1</b>
+              </span>
+            </p>
+          </div>
+        </div>
 
-    
+        <div
+          class="card"
+          onClick={() => {
+            handleClick("Loader");
+          }}
+          title="Loader"
+        >
+          <img class="card-img-top" src={img_avatar} alt="Avatar" />
+          <div class="card-body p-0 m-0">
+            <p class="text-center">
+              <span>
+                <b>TEST2</b>
+              </span>
+            </p>
+          </div>
+        </div>
 
+        <div
+          class="card"
+          onClick={() => {
+            handleClick("Fuel Burn");
+          }}
+          title="Fuel Burn"
+        >
+          <img class="card-img-top" src={img_avatar} alt="Avatar" />
+          <div class="card-body p-0 m-0">
+            <p class="text-center">
+              <span>
+                <b>TEST3</b>
+              </span>
+            </p>
+          </div>
+        </div>
 
-    
-        return(
+        <div
+          class="card"
+          onClick={() => {
+            handleClick("Task Manager");
+          }}
+          title="Task Manager"
+        >
+          <img class="card-img-top" src={img_avatar} alt="Avatar" />
+          <div class="card-body p-0 m-0">
+            <p class="text-center">
+              <span>
+                <b>TEST4</b>
+              </span>
+            </p>
+          </div>
+        </div>
 
-          
-<React.Fragment>
-  
+        <div
+          class="card"
+          onClick={() => {
+            handleClick("Publisher");
+          }}
+          title="Publisher"
+        >
+          <img class="card-img-top" src={img_avatar} alt="Avatar" />
+          <div class="card-body p-0 m-0">
+            <p class="text-center">
+              <span>
+                <b>TEST5</b>
+              </span>
+            </p>
+          </div>
+        </div>
 
-  
-<div className="wrapper">
-<div class="card" onClick={()=>{handleClick("Designer")}} title="Designer">
-      <img class="card-img-top" src={img_avatar} alt="Avatar" />
-      <div class="card-body p-0 m-0">
-          <p class="text-center"><span><b>Designer</b></span></p>
+        <div
+          class="card"
+          onClick={() => {
+            handleClick("Caterer Portal");
+          }}
+          title="Caterer Portal"
+        >
+          <img class="card-img-top" src={img_avatar} alt="Avatar" />
+          <div class="card-body p-0 m-0">
+            <p class="text-center">
+              <span>
+                <b>TEST6</b>
+              </span>
+            </p>
+          </div>
+        </div>
+
+        <div
+          class="card"
+          onClick={() => {
+            handleClick("Reference Data");
+          }}
+          title="Reference Data"
+        >
+          <img class="card-img-top" src={img_avatar} alt="Avatar" />
+          <div class="card-body p-0 m-0">
+            <p class="text-center">
+              <span>
+                <b>TEST7</b>
+              </span>
+            </p>
+          </div>
+        </div>
+
+        <div
+          class="card"
+          onClick={() => {
+            handleClick("Image Library");
+          }}
+          title="Image Library"
+        >
+          <img class="card-img-top" src={img_avatar} alt="Avatar" />
+          <div class="card-body p-0 m-0">
+            <p class="text-center">
+              <span>
+                <b>TEST8</b>
+              </span>
+            </p>
+          </div>
+        </div>
+
+        <div
+          class="card"
+          onClick={() => {
+            handleClick("Part Catelog");
+          }}
+          title="Part Catelog"
+        >
+          <img class="card-img-top" src={img_avatar} alt="Avatar" />
+          <div class="card-body p-0 m-0">
+            <p class="text-center">
+              <span>
+                <b>TEST9</b>
+              </span>
+            </p>
+          </div>
+        </div>
+
+        <div
+          class="card"
+          onClick={() => {
+            handleClick("Container Catelog");
+          }}
+          title="container Catelog"
+        >
+          <img class="card-img-top" src={img_avatar} alt="Avatar" />
+          <div class="card-body p-0 m-0">
+            <p class="text-center">
+              <span>
+                <b>TEST10</b>
+              </span>
+            </p>
+          </div>
+        </div>
+
+        <div
+          class="card"
+          onClick={() => {
+            handleClick("Preferences");
+          }}
+          title="Preferences"
+        >
+          <img class="card-img-top" src={img_avatar} alt="Avatar" />
+          <div class="card-body p-0 m-0">
+            <p class="text-center">
+              <span>
+                <b>TEST11</b>
+              </span>
+            </p>
+          </div>
+        </div>
+
+        <div
+          class="card"
+          onClick={() => {
+            handleClick("Tools");
+          }}
+          title="Tools"
+        >
+          <img class="card-img-top" src={img_avatar} alt="Avatar" />
+          <div class="card-body p-0 m-0">
+            <p class="text-center">
+              <span>
+                <b>TEST12</b>
+              </span>
+            </p>
+          </div>
         </div>
       </div>
-
-
-
-<div class="card" onClick={()=>{handleClick("Loader")}} title="Loader">
-      <img class="card-img-top" src={img_avatar} alt="Avatar" />
-      <div class="card-body p-0 m-0">
-          <p class="text-center"><span><b>Loader</b></span></p>
-        </div>
-      </div>
-
-  
-<div class="card" onClick={()=>{handleClick("Fuel Burn")}} title="Fuel Burn">
-      <img class="card-img-top" src={img_avatar} alt="Avatar" />
-      <div class="card-body p-0 m-0">
-          <p class="text-center"><span><b>Fuel Burn</b></span></p>
-        </div>
-      </div>
-  
-<div class="card" onClick={()=>{handleClick("Task Manager")}} title="Task Manager">
-      <img class="card-img-top" src={img_avatar} alt="Avatar" />
-      <div class="card-body p-0 m-0">
-          <p class="text-center"><span><b>Task Manager</b></span></p>
-        </div>
-      </div>
-
-
-<div class="card" onClick={()=>{handleClick("Publisher")}} title="Publisher">
-      <img class="card-img-top" src={img_avatar} alt="Avatar" />
-      <div class="card-body p-0 m-0">
-          <p class="text-center"><span><b>Publisher</b></span></p>
-        </div>
-      </div>
-
- 
-
-
-<div class="card" onClick={()=>{handleClick("Caterer Portal")}}  title="Caterer Portal">
-      <img class="card-img-top" src={img_avatar} alt="Avatar" />
-      <div class="card-body p-0 m-0">
-          <p class="text-center"><span><b>Caterer Portal</b></span></p>
-        </div>
-      </div>
-
-
-
-<div class="card" onClick={()=>{handleClick("Reference Data")}}  title="Reference Data">
-      <img class="card-img-top" src={img_avatar} alt="Avatar" />
-      <div class="card-body p-0 m-0">
-          <p class="text-center"><span><b>Reference Data</b></span></p>
-        </div>
-      </div>
-
-
-
-<div class="card" onClick={()=>{handleClick("Image Library")}}  title="Image Library">
-      <img class="card-img-top" src={img_avatar} alt="Avatar" />
-      <div class="card-body p-0 m-0">
-          <p class="text-center"><span><b>Image Library</b></span></p>
-        </div>
-      </div>
-
-<div class="card" onClick={()=>{handleClick("Part Catelog")}}  title="Part Catelog">
-      <img class="card-img-top" src={img_avatar} alt="Avatar" />
-      <div class="card-body p-0 m-0">
-          <p class="text-center"><span><b>Part Catelog</b></span></p>
-        </div>
-      </div>
-
-
-
-
-<div class="card" onClick={()=>{handleClick("Container Catelog")}} title="container Catelog">
-      <img class="card-img-top" src={img_avatar} alt="Avatar" />
-      <div class="card-body p-0 m-0">
-          <p class="text-center"><span><b>container Catelog</b></span></p>
-        </div>
-      </div>
-
-     
-
-<div class="card"  onClick={()=>{handleClick("Preferences")}}  title="Preferences">
-      <img class="card-img-top" src={img_avatar} alt="Avatar" />
-      <div class="card-body p-0 m-0">
-          <p class="text-center"><span><b>Preferences</b></span></p>
-        </div>
-      </div>
-
-
-<div class="card"  onClick={()=>{handleClick("Tools")}}  title="Tools">
-      <img class="card-img-top" src={img_avatar} alt="Avatar" />
-      <div class="card-body p-0 m-0">
-          <p class="text-center"><span><b>Tools</b></span></p>
-        </div>
-      </div>
-</div>
-</React.Fragment>
-
-        );
-
-    }
+    </React.Fragment>
+  );
+};
 export default GpGridLayout;
